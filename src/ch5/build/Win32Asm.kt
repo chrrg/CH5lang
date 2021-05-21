69a530c8236409adb34cd95b96e7cce9409a0361
caohong <877562884admin>
2021-05-21 19:11:33 +0800

    dev

---
+++ src/ch5/build/Win32Asm.kt
@@ -0,0 +1,159 @@
+package ch5.build
+
+fun push(value: Int): CodeItem {
+    val result = CodeItem()
+    result.byte(0x68)
+    result.dword(value)
+    return result
+}
+
+fun push(addr: Addr): CodeItem {
+    val result = CodeItem()
+    result.byte(0xFF)
+    if (addr.value == 0) {
+        addr.register?.let {
+            result.byte(0x30 + it.value)
+        } ?: run {
+            throw Exception("?")
+        }
+    } else {
+        addr.register?.let {
+            result.byte(0xB0 + it.value)
+        } ?: run {
+            result.byte(0x35)
+        }
+        result.dword(addr.value)
+    }
+    return result
+}
+
+fun mov(addr: Addr, register: Win32Register): CodeItem {
+    val result = CodeItem()
+    result.byte(0x89)
+    addr.register?.let {
+        result.byte(0x80 + it.value + register.value * 8)
+    } ?: run {
+        result.byte(0x05 + register.value * 8)
+    }
+    result.dword(addr.value, "addr")
+    if (addr is AddrSection) {
+        result.fix(0, "addr", fun(_: Int): Int {
+            return 0x400000 + virtualAddressOf(addr.parentSection) + addr.parentSection.offset(addr.section)
+        })
+    }
+    return result
+}
+
+fun mov(register: Win32Register, value: Int): CodeItem {
+    val result = CodeItem()
+    result.byte(0xB8 + register.value)
+    result.dword(value)
+    return result
+}
+
+fun mov(register: Win32Register, register2: Win32Register): CodeItem {
+    val result = CodeItem()
+    result.byte(0x8B)
+    result.byte(0xC0 + register.value * 8 + register2.value)
+    return result
+}
+
+fun mov(register: Win32Register, addr: Addr): CodeItem {
+    val result = CodeItem()
+    if (addr.value == 0 && addr !is AddrSection) {
+        result.byte(0x8B)
+        result.byte(register.value * 8 + addr.register!!.value)
+    } else {
+        addr.register?.let {
+//            mov eax,[eax+1] 8B 40 01
+//            mov eax,[eax+0x111] 8B 80 11 01 00 00
+            result.byte(0x8B)
+            result.byte(0x80 + register.value * 8 + it.value)
+            result.dword(addr.value, "addr")
+        } ?: run {
+//            mov eax,[0x111] A1 11 01 00 00
+//            mov ecx,[0x111] 8B 0D 11 01 00 00
+//            mov edx,[0x111] 8B 15 11 01 00 00
+//            mov ebx,[0x111] 8B 1D 11 01 00 00
+            if (register == EAX) {
+                result.byte(0xA1)
+                result.dword(addr.value, "addr")
+            } else {
+                result.byte(0x8B)
+                result.byte(0x05 + register.value * 8)
+                result.dword(addr.value, "addr")
+            }
+        }
+        if (addr is AddrSection) {
+            result.fix(0, "addr", fun(_: Int): Int {
+                return 0x400000 + virtualAddressOf(addr.parentSection) + addr.parentSection.offset(addr.section)
+            })
+        }
+    }
+    return result
+}
+
+class Invoke(ili: ImportLibraryItem) : CodeItem() {
+    init {
+        word(0x15ff)
+        dword(0, "invoke")
+        fix(0, "invoke", fun(value: Int): Int {
+            return 0x400000 + virtualAddressOf(ili.importManager!!.idataSection!!) + ili.offset
+        })
+    }
+}
+
+class Call(fn: Fun) : CodeItem() {
+    init {
+        byte(0xE8)
+        dword(0, "call")
+        fix(0, "call", fun(value: Int): Int {
+            return fn.offset - getCodeOffset() - 5
+        })
+    }
+}
+
+open class Win32Register(val value: Int)
+object EAX : Win32Register(0)
+object ECX : Win32Register(1)//this
+object EDX : Win32Register(2)
+object EBX : Win32Register(3)
+object ESP : Win32Register(4)//堆栈的最顶端
+object EBP : Win32Register(5)
+object ESI : Win32Register(6)
+object EDI : Win32Register(7)
+
+//EIP：指向CPU下一步即将执行的指令
+fun push(register: Win32Register): CodeItem {
+    val result = CodeItem()
+    result.byte(0x50 + register.value)
+    return result
+}
+
+fun pop(register: Win32Register): CodeItem {
+    val result = CodeItem()
+    result.byte(0x58 + register.value)
+    return result
+}
+
+fun jz(code: CodeBox): CodeItem {
+    val result = CodeItem()
+    result.byte(0x0F)
+    result.byte(0x84)
+    result.dword(0, "jz")
+    result.fix(0, "jz", fun(_): Int {
+        return code.getSize()
+    })
+    return result
+}
+
+fun jnz(code: CodeBox): CodeItem {
+    val result = CodeItem()
+    result.byte(0x0F)
+    result.byte(0x85)
+    result.dword(0, "jnz")
+    result.fix(0, "jnz", fun(_): Int {
+        return code.getSize()
+    })
+    return result
+}
--- src/ch5/parser/Parser.kt
+++ src/ch5/parser/Parser.kt
@@ -3,37 +3,63 @@ package ch5.parser
 import ch5.ast.ASTContainer
 import ch5.build.*
 
+open class BuildObject {
+    val size = 0//对象的字节大小
+    val func = ArrayList<CodeBox>()
+
+}
+
+class BuildClass : BuildObject()
 object Parser {
     fun parse(ast: ASTContainer): BuildStruct {
         val app = BuildStruct()
-        val code = app.codeSection
-        val exitProcess = app.importManager.use("KERNEL32.DLL", "ExitProcess")
+        val buildList = ArrayList<BuildObject>()
 
-//        ast.container
+        buildList.add(BuildObject())
+        val code = app.codeSection
+
         //写入调用主函数的代码
         val getProcessHeap = app.importManager.use("KERNEL32.DLL", "GetProcessHeap")
         val heapAlloc = app.importManager.use("KERNEL32.DLL", "HeapAlloc")
 
         val heap = AddrSection(app.dataSection.add(DwordSection(0)), app.dataSection)//堆空间开始地址
-        val entryStatic = AddrSection(app.dataSection.add(DwordSection(0)), app.dataSection)//堆空间开始地址
-
+//        val entryStatic = AddrSection(app.dataSection.add(DwordSection(0)), app.dataSection)//堆空间开始地址
+//
         Invoke(getProcessHeap).addTo(code)//获取程序堆
         mov(heap, EAX).addTo(code) //将eax存入heap中
+//
+//        // 调用主函数先要实例化主函数所在的static对象 初始化静态对象
+//
+//        push(32).addTo(code) // dwBytes是分配堆内存的大小。
+//        push(8).addTo(code) // dwFlags是分配堆内存的标志。包括HEAP_ZERO_MEMORY，即使分配的空间清零。
+//        mov(EAX, heap).addTo(code) //将heap取出到eax
+//
+//        push(EAX).addTo(code) // hHeap是进程堆内存开始位置。
+//        Invoke(heapAlloc).addTo(code)//获取程序堆
+//        mov(entryStatic, EAX).addTo(code) // 将eax放到程序data段内
 
-        // 调用主函数先要实例化主函数所在的static对象 初始化静态对象
-
-        push(32).addTo(code) // dwBytes是分配堆内存的大小。
-        push(8).addTo(code) // dwFlags是分配堆内存的标志。包括HEAP_ZERO_MEMORY，即使分配的空间清零。
-        mov(EAX, heap).addTo(code) //将heap取出到eax
-
-        push(EAX).addTo(code) // hHeap是进程堆内存开始位置。
-        Invoke(heapAlloc).addTo(code)//获取程序堆
-        mov(entryStatic, EAX).addTo(code) // 将eax放到程序data段内
-
-
+        val exitProcess = app.importManager.use("KERNEL32.DLL", "ExitProcess");
         push(0).addTo(code);Invoke(exitProcess).addTo(code)//最后写退出程序
-        //todo 写入各个会使用到的类和对象的方法
+        // todo 写入各个会使用到的类和对象的方法
 
+        //写入所有static对象的函数
+        for (i in buildList) {
+            val staticAddress = AddrSection(app.dataSection.add(DwordSection(0)), app.dataSection)//堆空间开始地址
+            val initMe = Fun()
+            // 判断是否为0 不为0则直接ret
+            mov(EAX, staticAddress).addTo(initMe)
+            val initCode = CodeBox()
+            push(32).addTo(initCode) // dwBytes是分配堆内存的大小。
+            push(8).addTo(initCode) // dwFlags是分配堆内存的标志。包括HEAP_ZERO_MEMORY，即使分配的空间清零。
+            mov(EAX, heap).addTo(initCode) //将heap取出到eax
+            push(EAX).addTo(initCode) // hHeap是进程堆内存开始位置。
+            Invoke(heapAlloc).addTo(initCode)//分配空间
+            mov(staticAddress, EAX).addTo(initCode) // 将eax存入对象的地址
+
+            jnz(initCode).addTo(initMe)//如果不是0就跳转到最后面
+            initMe.addTo(code)
+
+        }
 
         return app
     }
